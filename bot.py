3cfdcf26b70315a9232cbb03336b761246dab4bc
johnat95 <95886716+johnat95@users.noreply.github.com>
2022-10-24 20:31:57 -0700

    ADDED: loging via .env

--- bot.py
+++ bot.py
@@ -2,15 +2,31 @@ import asyncio
 import discord
 from discord.ext import commands
 import os
-import json
 
+from dotenv import load_dotenv
+
+#get app working directory and append env file name
+env_path =  os.path.abspath(os.path.dirname(__file__) + "/bot.env")
+
+#load enviroment variables
+load_dotenv(env_path)
+
+#get bot token
+token = str(os.getenv("TOKEN"))
+
+#location of cog classes
 cogs_dir = './cogs/'
 
+#command prefix, 
 prefix = "$"
 
 intent = discord.Intents.default()
 
 bot = commands.Bot(intents=intent, command_prefix=prefix)
+
+@bot.event
+async def on_ready(self):
+    print("Bot Online and Fully Functional")
 
 
 async def load_ext():
@@ -19,13 +35,20 @@ async def load_ext():
         if (file.endswith('.py')):
 
             await bot.load_extension(F'cogs.{file[:-3]}')
-            print(file[:-3])
+            print("Loading " + file)
 
 
 async def main():
-    print("loading exts")
+    print("Loading Extensions...")
 
     await load_ext()
+    try:
+        await bot.start(token)
+        
+    except discord.HTTPException as ex:
+        print(repr(ex))
 
 
 asyncio.run(main())
+
+
