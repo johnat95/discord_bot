bd9851c4ba5879bf241fb338e8457a7814e57d30
johnat95 <95886716+johnat95@users.noreply.github.com>
2022-10-24 22:41:04 -0700

    ADDED: comments to bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ token = str(os.getenv("TOKEN"))
 #location of cog classes
 cogs_dir = './cogs/'
 
-#command prefix, 
+#command prefix 
 prefix = "$"
 
 intent = discord.Intents.default()
@@ -30,7 +30,13 @@ intent = discord.Intents.default()
 bot = commands.Bot(intents=intent, command_prefix=prefix)
 
 async def load_ext():
-    #get a list of the files in the ./cog folder
+    """get a list of the files in the ./cog folder, these files each contain a class which inherits
+    the commands.Cog class. These classes contain collections of methods decorated with @commands.command,
+    which can be called froma direct message to the bot, leading with the command prefix.
+
+    ie. $method_name args
+    """ 
+
     for file in os.listdir(cogs_dir):
 
         if (file.endswith('.py')):
@@ -48,6 +54,7 @@ async def main():
         await bot.start(token)
         
     except discord.HTTPException as ex:
+        #prints a string representing the error
         print(repr(ex))
 
 
